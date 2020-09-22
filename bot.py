1339560f9d5cecf6004ec70e4ed42ce45e12e808
iris <iris@serv.pink>
2020-09-22 23:35:48 +0000

    Add !info command set; ignore automated queries for #request-feed

--- bot.py
+++ bot.py
@@ -11,25 +11,20 @@ from oil import oil
 client = discord.Client()
 db = oil.open()
 
-@client.event
-async def on_ready():
-	print('We have logged in as {0.user}'.format(client))
+API_PREFIX = 'https://fic.pw/api/v0/epub?automated=true&q='
 
-@client.event
-async def on_message(message):
-	print(json.dumps({"message":{
-		"author":str(message.author),
-		"content":str(message.content),
-	}}))
-	if message.author == client.user:
-		return
-
-	if message.content.startswith('!test'):
-		await message.channel.send('Hello!')
+def lookup(query: str):
+	import requests
+	try:
+		req = requests.get(API_PREFIX + query)
+		res = req.json()
+		return res
+	except:
+		return {'error':-1,'msg':'lookup failed :('}
 
 class RequestLog:
 	def __init__(self, id_, created_, infoRequestMs_, epubCreationMs_, urlId_,
-			query_, ficInfo_, epubFileName_, hash_, url_):
+			query_, ficInfo_, epubFileName_, hash_, url_, isAutomated_):
 		self.id = id_
 		self.created = created_
 		self.infoRequestMs = infoRequestMs_
@@ -40,6 +35,7 @@ class RequestLog:
 		self.epubFileName = epubFileName_
 		self.hash = hash_
 		self.url = url_
+		self.isAutomated = isAutomated_
 
 	@staticmethod
 	def maxId() -> int:
@@ -56,6 +52,87 @@ class RequestLog:
 			ls = [RequestLog(*r) for r in curs.fetchall()]
 			return ls
 		return []
+
+	@staticmethod
+	def mostRecentByUrlId(urlId):
+		with db.cursor() as curs:
+			curs.execute('''
+			select * from requestLog
+			where urlId = %s
+			order by created desc limit 1''', (urlId,))
+			r = curs.fetchone()
+			if r is None:
+				return None
+			return RequestLog(*r)
+
+@client.event
+async def on_ready():
+	print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+	print(json.dumps({"message":{
+		"author":str(message.author),
+		"content":str(message.content),
+	}}))
+	if message.author == client.user:
+		return
+
+	if message.content.startswith('!test'):
+		await message.channel.send('Hello!')
+	infoCommandPrefixes = ['lookup', 'info', 'epub', 'link']
+	for pre in infoCommandPrefixes:
+		if message.content.startswith(f"!{pre}"):
+			query = message.content[len(f"!{pre}"):].strip()
+			break
+	else:
+		return
+	query = query.strip('<>| \t').strip()
+	await message.add_reaction('👍')
+
+	try:
+		fut = asyncio.get_event_loop().run_in_executor(None, lookup, query)
+		res = await fut
+	except Exception as e:
+		await message.channel.send(f"unable to lookup '{query}'")
+		await message.add_reaction('❌')
+		traceback.print_exc()
+		print(e)
+		print('watch_requests: error: ^')
+
+	# FIXME why do we need this client param?
+	await message.remove_reaction('👍', client.user)
+	if res is None or 'urlId' not in res \
+			or ('error' in res and int(res['error']) != 0):
+		await message.channel.send(f"unable to lookup '{query}'")
+		await message.add_reaction('❌')
+		print(res)
+		return
+
+	try:
+		l = RequestLog.mostRecentByUrlId(res['urlId'])
+		url = urllib.parse.urljoin('https://fic.pw/', l.url)
+		info = json.loads(l.ficInfo)
+		descSoup = BeautifulSoup(info['desc'], 'lxml')
+		infoTime = f'{l.infoRequestMs/1000.0:.3f}s'
+		epubTime = f'{l.epubCreationMs/1000.0:.3f}s'
+		msg = '\n'.join([
+				f'request for <{l.query}> => `{l.urlId}` ({infoTime}), generated epub in {epubTime}',
+			])
+		title = f'{info["title"]} by {info["author"]}'
+		desc = '\n'.join([
+				f'{descSoup.get_text()}',
+				f'{info["words"]} words in {info["chapters"]} chapters',
+			])
+		e = discord.Embed(title=title, description=desc, url=url)
+		await message.channel.send(msg, embed=e)
+		await message.add_reaction('✅')
+	except Exception as e:
+		await message.channel.send(f"unable to find lookup result for '{query}'")
+		await message.add_reaction('❌')
+		traceback.print_exc()
+		print(e)
+		print('watch_requests: error: ^')
 
 def pruneRecent(recent, minutes=15):
 	nr = {}
@@ -91,6 +168,10 @@ async def watch_requests():
 					f'<{url}> (`{l.hash}`)',
 				])
 			await botspam_priv.send(msg)
+
+			if l.isAutomated:
+				continue
+
 			try:
 				info = json.loads(l.ficInfo)
 				descSoup = BeautifulSoup(info['desc'], 'lxml')
